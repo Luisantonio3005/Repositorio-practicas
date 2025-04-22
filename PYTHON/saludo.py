f36aaf327b9b7bca2214e10ef983de0b2e53e6ca
Luisantonio3005 <luisantoniocanalesguerrero32@gmail.com>
2025-04-22 12:52:09 -0600

    Subiendo mis proyectos hechos a git commit 1

---
+++ PYTHON/saludo.py
@@ -0,0 +1,6 @@
+Nombre = "Luis Antonio"
+Apellido = "Canales Guerrero"
+Espacio= " "
+print("Hola" + Espacio + "mi nombre es" + Espacio + Nombre + Espacio + Apellido)
+saludo = print
+print(saludo)
---
+++ PYTHON/suma.py
@@ -0,0 +1,6 @@
+a=2
+b=8.2
+c=50*27
+d= a+b+c
+print("la suma es:",d)
+
